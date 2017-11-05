1f748d9872118f6c35e66c85425d174c0db85171
maxjacobson <max.jacobson@torchlms.com>
2017-11-04 21:01:20 -0600

    clean up app folders

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Routes from './Components/Routes';
 import './index.css';
-import App from './App';
+import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './clean-blog.min.css';
 import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+    
+      render() {
+        return (
+          <div>
+            <Routes />
+          </div>
+        );
+      }
+    }
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
